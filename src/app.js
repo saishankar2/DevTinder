b9b58cecc661c11cc9904def2663b844b8537cb3
saishankar2 <shankarpatibandla22@gmail.com>
2025-05-02 00:52:03 +0530

    Middlewares & Error Handling

--- src/app.js
+++ src/app.js
@@ -12,27 +12,49 @@ const app = express();
 //     }
 // })// this order also matters as it is middleware and is executed in the order it is defined
 
-app.get("/user/:id", (req, res)=>{  //handles the dynamic params like user/60
-    console.log(req.params); // read the query params
-    res.send({fs: "sai", ls:"shankar"})
-})
+// app.get("/user/:id", (req, res)=>{  //handles the dynamic params like user/60
+//     console.log(req.params); // read the query params
+//     res.send({fs: "sai", ls:"shankar"})
+// })
 
-app.get("/user", (req, res)=>{  // just handles the /user path's get function
-    console.log(req.query); // read the query params
-    res.send({fs: "sai", ls:"shankar"})
-})
+// app.get("/user", (req, res)=>{  // just handles the /user path's get function
+//     console.log(req.query); // read the query params
+//     res.send({fs: "sai", ls:"shankar"})
+// })
 
-app.post("/user", (req, res)=>{  // just handles the /user path's post function
-    console.log("Saved");
-    res.send({fs: "Virat", ls:"Kohli"})
-})
+// app.post("/user", (req, res)=>{  // just handles the /user path's post function
+//     console.log("Saved");
+//     res.send({fs: "Virat", ls:"Kohli"})
+// })
 
-app.use("/hello",(req, res)=>{
-    res.send("Hello World!")       // this function is called request handler
-})
+// app.use("/hello",(req, res)=>{
+//     res.send("Hello World!")       // this function is called request handler
+// })
 
-app.use("/test", (req, res)=>{
-    res.send("You can test here!")       // this function is called request handler
+// app.use("/test", (req, res)=>{
+//     res.send("You can test here!")       // this function is called request handler
+// })
+
+app.use("/user",
+    [(req,res,next)=>{
+        //throw new Error("Testing error handling"); //Using this to test wildcard error handling at the end
+        console.log("Hey");
+        res.status(200).send("Response 1");   // In this case only the first fn runs, no matter the response is sent or not
+        next();
+    },                            // For acheiving this mechanism we use next() and we can only send any one response
+    (req, res)=>{                 //Even if the next is placed before the res.send we will still get that error(Cannot set headers after they are sent to the client) as the 2nd response will be sent and the 1st one won't
+        console.log("Hi");      // If all the request handlers just calls next(), without any response, then we get an error as there will be no handler that sends a response
+        res.send("Response 2"); //i.e the response should be sent
+    }                          
+])  // We can wrap the handlers in an array either all or just a few but the behaviour is the same.. app.use("path", [r1, r2], r3, r4)
+// A good practice is to use middlewares in a separate file and just require and use the function here and using try catch is also good for api calls
+
+app.use("/", (err, req, res, next)=>{  //err should be the first param when used
+    if(err){
+        //You can log your error here
+        res.status(500).send("Something's broken"); //Good way to gracefully send an error
+    }
+//the wild card error handling should be on the end of all the handlers as if some handler throws an error it catches it
 })
 
 app.listen(3000, ()=>{
